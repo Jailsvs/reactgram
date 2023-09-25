585089129021c024e5074d181b14d401fd321854
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-09-25 07:12:18 -0300

    commit inicial projeto

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+const conn = async () => {
+  try {
+    console.log(dbPass);
+    const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.mbc8dst.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp`);
+    console.log("Conectado ao banco de dados Mongo Atlas DB");
+    return dbConn;
+  } catch (error) {
+    console.log("Erro de conexão mongoose");
+    console.log(error);
+  }
+};
+
+conn();
+
+module.exports = conn;
