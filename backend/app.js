eb80085efa0348929454f0111f76889aaba70f4a
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-09-25 07:47:36 -0300

    Inicio middleware validation

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,15 @@ app.use(express.urlencoded({extended: false}));
 
 app.use(cors({credentials: true, origin: "http://localhost:3000"}));
 
+/*app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});*/
+
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 require("./config/db.js");
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const {register} = require("../controllers/UserController")
 
-router.get("/register", register);
+const validate = require("../middlewares/handleValidation");
+router.post("/register", validate, register);
 
 module.exports = router;
