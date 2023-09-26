449ca3e7ade4326dbd04c43e4b34420343467eeb
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-09-26 07:52:17 -0300

    Ajustes app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
-require("dotenv").config()
-const express= require("express")
-const path = require("path")
-const cors = require("cors")
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const port = process.env.PORT;
 
 const app = express();
-app.use(express.json);
-app.use(express.urlencoded({extended: false}));
+// Config JSON and form data response
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({credentials: true, origin: "http://localhost:3000"}));
 
@@ -24,7 +26,11 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 require("./config/db.js");
 
-const router = require("./routes/Router");
+app.get("/", (req, res) => {
+  res.send("API Working!");
+});
+
+const router = require("./routes/Router.js");
 app.use(router);
 
 app.listen(port, () => {
