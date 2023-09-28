608fec2bc495238965622fd303f55defb7ebfb32
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-09-28 07:37:38 -0300

    Continuacao com login user controller e middleware authguard

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
-const {register} = require("../controllers/UserController")
+// Controller
+const {
+  register,
+  getCurrentUser,
+  login,
+  update,
+  getUserById,
+} = require("../controllers/UserController");
 
-const validate = require("../middlewares/handleValidation");
-const {userCreateValidation} = require("../middlewares/userValidations")
+// Middlewares
+const validate = require("../middlewares/handleValidations");
+const {
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation,
+} = require("../middlewares/userValidations");
+const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
 
+// Routes
 router.post("/register", userCreateValidation(), validate, register);
+router.get("/profile", authGuard, getCurrentUser);
+router.post("/login", loginValidation(), validate, login);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
+router.get("/:id", getUserById);
 
 module.exports = router;
