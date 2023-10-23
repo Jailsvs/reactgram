b8b09a43213dc30109f62dac387888b38077e9fe
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-10-23 07:48:58 -0300

    componente Message para errors

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -8,9 +8,10 @@ const register = async (data) => {
     .then((res) => res.json())
     .catch((err) => err);
 
-    if (res) {
+    if (res) 
       localStorage.setItem("user", JSON.stringify(res));
-    }
+    
+    return res;
   } catch (error) {
     console.log(error)
   }
